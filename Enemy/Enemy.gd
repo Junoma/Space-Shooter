c9e6da4dacb0134e57e5251c3528d9c8bacc4e80
Junoma <142906674+Junoma@users.noreply.github.com>
2023-09-25 18:40:04 -0400

    2.4 Menu and HUD
    Updated Menu and HUD code

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -1,6 +1,19 @@
 extends CharacterBody2D
 
 var Bullet = load("res://Enemy/enemy_bullet.tscn")
+var direction = Vector2(1.5,0)
+var wobble = 30.0
+
+func _ready():
+	initial_position.x = -100
+	initial_position.y = y_positions[randi() % y+positions.size()]
+	position = initial_position
+	
+func _physics_process(_delta):
+	position += direction 
+	position.y = initial_position.y +sin(posision.x/20)*wobble
+	if position.x > Global.VP.x + 100:
+		queue_free()
 
 
 func _on_timer_timeout():
@@ -13,3 +26,14 @@ func _on_timer_timeout():
 		bullet.rotation = d
 		bullet.global_position = global_position + Vector2(0,-260).rotated(d)
 		Effects.add_child(bullet)
+
+func damage(d):
+	health -= d
+	if health <= 0: 
+		Global.update_score(500)
+	queue_free()
+	
+func _on_area_2d_body_enetered(body):
+		if body.name == "Player":
+			damage(100)
+			body.damage(100)
